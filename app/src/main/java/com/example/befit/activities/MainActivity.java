ee1fd545d627312816b24fc0db1bc384c62a0fbc
“Kolia” <kozlovskyy.m@nltu.lviv.ua>
2024-11-21 20:27:47 +0200

    first2

--- app/src/main/java/com/example/befit/activities/MainActivity.java
+++ app/src/main/java/com/example/befit/activities/MainActivity.java
@@ -7,16 +7,28 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.befit.databinding.ActivityMainBinding;
-
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    private FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+
+        // перевірка чи зареєстрований
+        if (auth.getCurrentUser() == null) {
+            // If not logged in, redirect to registration
+            startActivity(new Intent(this, RegActivity.class));
+            finish(); // Close current activity
+            return;
+        }
 
         binding.btnActivity.setOnClickListener(v -> {
             startActivity(new Intent(this, StatsActivity.class));
