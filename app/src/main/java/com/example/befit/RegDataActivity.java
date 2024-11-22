60fdfe4e183692ab3d8a7fbc5f27ee4282e71f57
“Kolia” <kozlovskyy.m@nltu.lviv.ua>
2024-11-22 19:50:34 +0200

    Initial commit

--- app/src/main/java/com/example/befit/RegDataActivity.java
+++ app/src/main/java/com/example/befit/RegDataActivity.java
@@ -6,9 +6,6 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.befit.databinding.ActivityRegDataBinding;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class RegDataActivity extends AppCompatActivity {
     private ActivityRegDataBinding binding;
